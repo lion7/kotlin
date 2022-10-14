34102a04d54e9c78d0b85c175d2be0be7ea4eb9c
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2022-10-14 11:47:04 +0200

    Break codegen test data for KT-49507 test
    Here we changed the test data to the state we should have after a proper
    fix of KT-54393. Now it doesn't pass on all backend.

--- compiler/testData/codegen/box/fieldRename/javaFieldAndKotlinProperty.kt
+++ compiler/testData/codegen/box/fieldRename/javaFieldAndKotlinProperty.kt
@@ -1,15 +1,18 @@
 // TARGET_BACKEND: JVM_IR
 // IGNORE_BACKEND: JVM_IR
+// See KT-54393 for details (fails with exception)
+// IGNORE_BACKEND_FIR: JVM_IR
+// FIR status: resolved to the field instead of property
 // DUMP_IR
 
 // FILE: BaseJava.java
 public class BaseJava {
-    public String a = "OK";
+    public String a = "FAIL";
 }
 
 // FILE: Derived.kt
 class Derived : BaseJava() {
-    val a = "FAIL"
+    val a = "OK"
 }
 
 fun box() = Derived().a
